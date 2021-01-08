c180b9c7f6a1e8f03e890ed5ee1d8442cca5d342
la4025 <ehdgns4025@gmail.com>
2021-01-08 12:26:32 +0900

    create customer.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,52 @@ import logo from './logo.svg';
 import './App.css';
 import Customer from './components/customer'
 
-const customer = {
+const customers = [
+{
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
   'name' : '나동훈',
   'birthday' : '950506',
   'gender' : '남자',
   'job' : '대학생'
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '홍길동',
+  'birthday' : '950506',
+  'gender' : '남자',
+  'job' : '프로그래머'
+},
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '황진이',
+  'birthday' : '950506',
+  'gender' : '여자',
+  'job' : '디자이너'
 }
+]
 
 function App() {
   return (
-    <Customer
-    name = {customer.name}
-    birthday = {customer.birthday}
-    gender = {customer.gender}
-    job = {customer.job}
-    />
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key = {c.id}
+              id = {c.id}
+              image = {c.image}
+              name = {c.name}
+              birthday = {c.birthday}
+              gender = {c.gender}
+              job = {c.job}
+            />
+          );
+        })
+      }
+    </div>
   );
 }
 

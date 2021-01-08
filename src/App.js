547bc37bcd6d55a56e89e460bfbb1b84c62dee91
la4025 <ehdgns4025@gmail.com>
2021-01-08 11:50:18 +0900

    1차 저장

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/customer'
+
+const customer = {
+  'name' : '나동훈',
+  'birthday' : '950506',
+  'gender' : '남자',
+  'job' : '대학생'
+}
 
 function App() {
   return (
-    <div className="gray-background">
-     <img src={logo} lat="logo"/>
-     <h2>Let's develop management system!</h2>
-    </div>
+    <Customer
+    name = {customer.name}
+    birthday = {customer.birthday}
+    gender = {customer.gender}
+    job = {customer.job}
+    />
   );
 }
 
---
+++ src/components/customer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Customer(props){
+    return(
+    <div>
+        <h2>{props.name}</h2>
+        <p>{props.birthday}</p>
+        <p>{props.gender}</p>
+        <p>{props.job}</p>
+    </div>
+    );
+}
+export default Customer;
